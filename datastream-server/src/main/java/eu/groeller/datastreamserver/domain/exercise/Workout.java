997a1d8a63c0a1fb7b9585761e9e6faa0f33deb1
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-11-11 22:46:45 +0100

    Add Workout/getWorkoutDuration

--- datastream-server/src/main/java/eu/groeller/datastreamserver/domain/exercise/Workout.java
+++ datastream-server/src/main/java/eu/groeller/datastreamserver/domain/exercise/Workout.java
@@ -3,32 +3,67 @@ package eu.groeller.datastreamserver.domain.exercise;
 import eu.groeller.datastreamserver.domain.AbstractEntity;
 import eu.groeller.datastreamserver.domain.User;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Null;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.val;
 
+import java.time.Duration;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "workout")
 public class Workout extends AbstractEntity {
-    
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
-    
+
     @Column(nullable = false)
     private Long duration;
-    
+
     @Column(nullable = false)
     private OffsetDateTime date;
-    
+
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "workout_id")
     private List<ExerciseRecord> exercises;
-    
+
     @Column(name = "average_rest_time")
     private Double averageRestTime;
+
+    public Workout(User user, OffsetDateTime date, List<ExerciseRecord> exercises) {
+        this.user = user;
+        this.date = date;
+        this.exercises = new ArrayList<>(exercises);
+
+        this.duration = getWorkoutDuration(this.exercises);
+    }
+
+    private Long getWorkoutDuration(List<ExerciseRecord> exercises) {
+        val exerciseList = exercises.stream()
+                .sorted(Comparator.comparingInt(ExerciseRecord::getOrder))
+                .collect(Collectors.toCollection(LinkedList::new));
+
+        val firstExercise = exerciseList.getFirst();
+        val lastExercise = exerciseList.getLast();
+
+        if (firstExercise == null) {
+            throw new NullPointerException("Exercise list has been wrongly created, first element is null");
+        }
+
+        if (firstExercise == lastExercise) {
+            return Duration.between(firstExercise.getStartTime(), firstExercise.getEndTime()).toMillis();
+        }
+        else {
+            return Duration.between(firstExercise.getStartTime(), lastExercise.getEndTime()).toMillis();
+        }
+    }
 }
