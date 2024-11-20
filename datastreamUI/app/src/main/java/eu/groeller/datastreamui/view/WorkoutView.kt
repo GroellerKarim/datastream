307da0c3ba56da0c82c3d789a238e8ebb0123b3f
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-11-20 15:36:37 +0100

    Add empty WorkoutView

---
+++ datastreamUI/app/src/main/java/eu/groeller/datastreamui/view/WorkoutView.kt
@@ -0,0 +1,9 @@
+package eu.groeller.datastreamui.view
+
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun workoutView() {
+}
