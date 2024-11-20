805825b7bb42484011f8c18d0760c34c311d470d
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-11-20 15:36:11 +0100

    Add MainScreen route to LoginScreen

--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/MainActivity.kt
+++ datastreamUI/app/src/main/java/eu/groeller/datastreamui/MainActivity.kt
@@ -1,17 +1,28 @@
 package eu.groeller.datastreamui
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import eu.groeller.datastreamui.ui.theme.DatastreamUITheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +30,44 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DatastreamUITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = MainMenu
+                ) {
+                    composable<MainMenu> {
+                        MainMenuScreen()
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+@Serializable
+object MainMenu
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    DatastreamUITheme {
-        Greeting("Android")
+fun MainMenuScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") }
+
+        )
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") }
+        )
     }
 }
+
+
