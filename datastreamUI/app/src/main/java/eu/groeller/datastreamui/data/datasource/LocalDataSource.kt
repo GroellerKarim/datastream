be3d0303ad535b01280d2524196963aa233c842d
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-11-25 18:46:01 +0100

    Add LocalDataSource writeUser method

--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/datasource/LocalDataSource.kt
+++ datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/datasource/LocalDataSource.kt
@@ -2,28 +2,40 @@ package eu.groeller.datastreamui.data.datasource
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import eu.groeller.datastreamui.User
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
 class LocalDataSource(private val dataStore: DataStore<Preferences>) {
 
+    val tokenKey = stringPreferencesKey("auth_token")
+    val usernameKey = stringPreferencesKey("username")
+    val emailKey = stringPreferencesKey("email")
+
     suspend fun readToken(): String? {
-        val tokenKey = stringPreferencesKey("auth_token")
         return dataStore.data.map { preferences -> preferences[tokenKey] }.first()
     }
 
     suspend fun readUser(): User? {
-        val tokenKey = stringPreferencesKey("auth_token")
         val tokenFlow =  dataStore.data.map { preferences -> preferences[tokenKey]}.first()
 
         if(tokenFlow == null) return null
 
-        val usernameFlow = dataStore.data.map { preferences -> preferences[stringPreferencesKey("username")] }
-        val emailFlow = dataStore.data.map { preferences -> preferences[stringPreferencesKey("email")] }
+        val usernameFlow = dataStore.data.map { preferences -> preferences[usernameKey] }
+        val emailFlow = dataStore.data.map { preferences -> preferences[emailKey] }
 
         return User(usernameFlow.first()!!, emailFlow.first()!!, tokenFlow)
+    }
+
+    suspend fun writeUser(user: User) {
+        dataStore.edit { preferencesDataStore ->
+            preferencesDataStore[tokenKey] = user.token
+            preferencesDataStore[usernameKey] = user.username
+            preferencesDataStore[emailKey] = user.email
+        }
     }
 }
