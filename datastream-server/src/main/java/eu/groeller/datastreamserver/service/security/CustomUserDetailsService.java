83c4e068d2cad1627c850a25914f4c58fea7b18e
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-12-02 22:26:10 +0100

    Add logging to CustomUserDetailsService

--- datastream-server/src/main/java/eu/groeller/datastreamserver/service/security/CustomUserDetailsService.java
+++ datastream-server/src/main/java/eu/groeller/datastreamserver/service/security/CustomUserDetailsService.java
@@ -3,11 +3,13 @@ package eu.groeller.datastreamserver.service.security;
 import eu.groeller.datastreamserver.configuration.security.CustomUserDetails;
 import eu.groeller.datastreamserver.persistence.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
@@ -15,9 +17,14 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        var user = userRepository.findById(Long.valueOf(email))
-                .orElseThrow(() -> new UsernameNotFoundException("User with id [" + email + "] not found"));
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+        log.debug("Searching for User with id [{}]", id);
+        var user = userRepository.findById(Long.valueOf(id))
+                .orElseThrow(() -> {
+                    log.warn("Did not find user with id [{}]!", id);
+                    return new UsernameNotFoundException("User with id [" + id + "] not found");
+                });
+        log.debug("Found user [{}]", user.getUsername());
         return new CustomUserDetails(user);
     }
 }
