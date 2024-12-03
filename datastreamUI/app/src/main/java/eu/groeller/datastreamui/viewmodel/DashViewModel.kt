b9ceacdfe54c5dcbcce41ebed7a2da9cd8659f52
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-12-03 17:56:25 +0100

    Add DashViewModel

--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/viewmodel/DashViewModel.kt
+++ datastreamUI/app/src/main/java/eu/groeller/datastreamui/viewmodel/DashViewModel.kt
@@ -3,16 +3,35 @@ package eu.groeller.datastreamui.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import eu.groeller.datastreamui.User
-import eu.groeller.datastreamui.data.UserRepository
-import kotlinx.coroutines.flow.Flow
+import eu.groeller.datastreamui.data.ErrorResponse
+import eu.groeller.datastreamui.data.user.UserRepository
+import eu.groeller.datastreamui.data.user.UserState
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class DashViewModel(private val userRepository: UserRepository): ViewModel() {
 
-    val user = userRepository.userStream
+    val userState = userRepository.userState
+
+    val uiState: StateFlow<UserState> = userState.stateIn(
+        scope = viewModelScope,
+        initialValue = UserState.Loading,
+        started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000)
+    )
+
+    fun login(email: String, password: String) {
+        viewModelScope.launch {
+            try {
+                userRepository.loginUser(email, password)
+                println("State changed successfully [${userState.first()}]")
+            } catch (e: Exception) {
+                println("Error changing state: ${e.message}")
+            }
+        }
+    }
 
 }
--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/viewmodel/DashViewState.kt
+++
@@ -1,9 +0,0 @@
-package eu.groeller.datastreamui.viewmodel
-
-import eu.groeller.datastreamui.User
-
-sealed interface DashViewState {
-    data object Loading: DashViewState
-    data class LoggedIn(val user: User): DashViewState
-    data class Failure(val message: String): DashViewState
-}
