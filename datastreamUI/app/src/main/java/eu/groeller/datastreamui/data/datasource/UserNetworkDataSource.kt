4eeb96d8a0063785e3c59a8b0cb1180361cc3d66
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-12-03 17:51:23 +0100

    Add UserNetworkDS/fetchUserWithToken api call

--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/datasource/UserNetworkDataSource.kt
+++ datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/datasource/UserNetworkDataSource.kt
@@ -1,8 +1,14 @@
 package eu.groeller.datastreamui.data.datasource
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import eu.groeller.datastreamui.User
+import eu.groeller.datastreamui.data.user.UserState
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.bearerAuth
+import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
@@ -11,7 +17,7 @@ import io.ktor.http.contentType
 
 class UserNetworkDataSource(private val httpClient: HttpClient) {
 
-
+    private lateinit var user: User
     private data class RegisterRequest(val username: String, val email: String, val password: String)
 
     suspend fun registerUser(username: String, email: String, password: String) {
@@ -39,5 +45,21 @@ class UserNetworkDataSource(private val httpClient: HttpClient) {
         throw Error()
     }
 
+    private data class FetchUserWithTokenResponse(val username: String, val email: String)
+    suspend fun fetchUserWithToken(token: String): UserState {
+        if(this::user.isInitialized) return UserState.Success(user)
+        val response = httpClient.get("/users") {
+            bearerAuth(token)
+        }
+
+        when (response.status) {
+            HttpStatusCode.OK -> {
+                val body: FetchUserWithTokenResponse = response.body()
+                this.user = User(body.username, body.email, token)
+                return UserState.Success(user)
+            }
+            else -> return UserState.NetworkFailed(response.body())
+        }
+    }
 
 }
