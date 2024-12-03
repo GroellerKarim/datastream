093d147a119924e1f7b1c726ea23783470c800fb
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-12-03 17:52:40 +0100

    Implement UserRepository_userState Flow

--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/user/UserRepository.kt
+++ datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/user/UserRepository.kt
@@ -1,13 +1,20 @@
-package eu.groeller.datastreamui.data
+package eu.groeller.datastreamui.data.user
 
-import eu.groeller.datastreamui.User
 import eu.groeller.datastreamui.data.datasource.LocalDataSource
 import eu.groeller.datastreamui.data.datasource.UserNetworkDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class UserRepository(private val localDataSource: LocalDataSource, private val userNetworkDataSource: UserNetworkDataSource) {
 
-    val userStream: Flow<User?> = localDataSource.userStream
+    val userState: Flow<UserState> = localDataSource.localUserStream.map { localUserState ->
+        when (localUserState) {
+            is LocalUserState.Loading -> UserState.Loading
+            is LocalUserState.LocalMissing -> UserState.LocalMissing
+            is LocalUserState.Success -> userNetworkDataSource.fetchUserWithToken(localUserState.token)
+        }
+    }
+
 
     suspend fun registerUser(username: String, email: String, password: String) {
         userNetworkDataSource.registerUser(username, email, password)
