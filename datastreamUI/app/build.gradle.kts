cb7690642837a87c6acf8865828e87f4bb420598
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-12-03 21:03:22 +0100

    Add ktor-serialization-json dependency

--- datastreamUI/app/build.gradle.kts
+++ datastreamUI/app/build.gradle.kts
@@ -67,5 +67,7 @@ dependencies {
     implementation(libs.ktor.client.okhttp)
 
     implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
 
 }
