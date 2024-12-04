d10e578c3cff226a46fff1c28d37175b63def957
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-12-04 23:45:26 +0100

    Add json-content-negotiation to HttpClientConfigurer

--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/HttpClientConfigurer.kt
+++ datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/HttpClientConfigurer.kt
@@ -3,6 +3,9 @@ package eu.groeller.datastreamui.data
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.ProxyBuilder
 import io.ktor.client.engine.http
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import java.net.InetSocketAddress
 import java.net.Proxy
 
@@ -13,6 +16,9 @@ class HttpClientConfigurer {
     val v1HttpClient: HttpClient = HttpClient() {
         engine {
             proxy = ProxyBuilder.http("http://192.168.0.81:$port/api/v1")
+        }
+        install(ContentNegotiation) {
+            json()
         }
     }
 
