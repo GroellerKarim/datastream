4140461bae9e099449bf8857185208b55d59c9dc
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-12-05 19:50:45 +0100

    Add baseUrl in HttpClientConfigurer

--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/HttpClientConfigurer.kt
+++ datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/HttpClientConfigurer.kt
@@ -1,24 +1,22 @@
 package eu.groeller.datastreamui.data
 
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.ProxyBuilder
-import io.ktor.client.engine.http
+import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
-import java.net.InetSocketAddress
-import java.net.Proxy
 
+val V1_PATH = "/api/v1"
 class HttpClientConfigurer {
 
     val port = 8080
+    private val baseUrl = "http://192.168.0.81:$port"
 
     val v1HttpClient: HttpClient = HttpClient() {
-        engine {
-            proxy = ProxyBuilder.http("http://192.168.0.81:$port/api/v1")
-        }
         install(ContentNegotiation) {
             json()
+        }
+        install(DefaultRequest) {
+            url(baseUrl)
         }
     }
 
