104836cfa6e0b42846e3748e39a1bc41d1f2f61c
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-12-05 19:52:59 +0100

    Fix paths in UserNetworkDataSource with V1_PATH global var

--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/datasource/UserNetworkDataSource.kt
+++ datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/datasource/UserNetworkDataSource.kt
@@ -1,9 +1,8 @@
 package eu.groeller.datastreamui.data.datasource
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import eu.groeller.datastreamui.User
+import eu.groeller.datastreamui.data.ErrorResponse
+import eu.groeller.datastreamui.data.V1_PATH
 import eu.groeller.datastreamui.data.user.UserState
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -11,44 +10,54 @@ import io.ktor.client.request.bearerAuth
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.request
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
+import kotlinx.serialization.Serializable
 
 class UserNetworkDataSource(private val httpClient: HttpClient) {
 
     private lateinit var user: User
-    private data class RegisterRequest(val username: String, val email: String, val password: String)
 
+    @Serializable
+    private data class RegisterRequest(val username: String, val email: String, val password: String)
     suspend fun registerUser(username: String, email: String, password: String) {
-        val response = httpClient.post("/users/regiser") {
+        val response = httpClient.post("$V1_PATH/users/register") {
             contentType(ContentType.Application.Json)
             setBody(RegisterRequest(username, email, password))
         }
         // TODO Handle Exceptions
     }
 
+    @Serializable
     private data class LoginRequest(val email: String, val password: String)
 
     suspend fun loginUser(email: String, password: String): User {
 
-        val response = httpClient.post("/users/login") {
+        val response = httpClient.post("$V1_PATH/users/login") {
             contentType(ContentType.Application.Json)
             setBody(LoginRequest(email, password))
         }
+        println(response.request.url)
 
         if (response.status == HttpStatusCode.OK) {
             // TODO Handle NoTransformationFoundException
             return response.body()
         }
-        // TODO, handle if backend not available, user with email does not exist or wrong password
+
+        println(response.status)
+        val s: ErrorResponse = response.body()
+        println(s)
         throw Error()
+        // TODO, handle if backend not available, user with email does not exist or wrong password
     }
 
+    @Serializable
     private data class FetchUserWithTokenResponse(val username: String, val email: String)
     suspend fun fetchUserWithToken(token: String): UserState {
         if(this::user.isInitialized) return UserState.Success(user)
-        val response = httpClient.get("/users") {
+        val response = httpClient.get("$V1_PATH/users") {
             bearerAuth(token)
         }
 
