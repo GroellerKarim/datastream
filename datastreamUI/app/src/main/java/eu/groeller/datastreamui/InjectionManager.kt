3765818d78d0f41430e7c9173eaf8e05d4993f96
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-12-06 15:47:48 +0100

    Add WorkoutRoute to MainActivity

--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/InjectionManager.kt
+++ datastreamUI/app/src/main/java/eu/groeller/datastreamui/InjectionManager.kt
@@ -6,12 +6,11 @@ import eu.groeller.datastreamui.data.HttpClientConfigurer
 import eu.groeller.datastreamui.data.user.UserRepository
 import eu.groeller.datastreamui.data.datasource.LocalDataSource
 import eu.groeller.datastreamui.data.datasource.UserNetworkDataSource
+import eu.groeller.datastreamui.viewmodel.DashViewModel
 
 class InjectionManager(private val datastore: DataStore<Preferences>) {
-
     val httpConfig = HttpClientConfigurer()
     val localDataSource: LocalDataSource = LocalDataSource(datastore)
     val userNetworkDataSource = UserNetworkDataSource(httpConfig.v1HttpClient)
     val userRepository = UserRepository(localDataSource, userNetworkDataSource)
-
 }
--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/MainActivity.kt
+++ datastreamUI/app/src/main/java/eu/groeller/datastreamui/MainActivity.kt
@@ -5,27 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import eu.groeller.datastreamui.screens.DashScreen
-import eu.groeller.datastreamui.ui.theme.DatastreamUITheme
+import eu.groeller.datastreamui.screens.LoginScreen
 import eu.groeller.datastreamui.viewmodel.DashViewModel
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
@@ -49,51 +39,17 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             NavHost(
                 navController = navController,
-                startDestination = DashScreenRoute
+                startDestination = DashRoute
             ) {
-                composable<DashScreenRoute> {
-                    DashScreen(DashViewModel(injectionHolder.userRepository))
+                composable<DashRoute> {
+                    DashScreen(DashViewModel(injectionHolder.userRepository), )
                 }
+                composable<WorkoutRoute> {  }
             }
         }
     }
 }
 
-@Serializable
-object LoginMenu
 
-@Serializable
-object DashScreenRoute
-
-@Composable
-fun MainMenuView(token: String) {
-    Column {
-        Text("Logged in with token: $token")
-    }
-}
-
-@Composable
-fun LoginMenuView(
-) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        var email by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
-        TextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") }
-
-        )
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") }
-        )
-    }
-}
-
-
+@Serializable object DashRoute
+@Serializable object WorkoutRoute
