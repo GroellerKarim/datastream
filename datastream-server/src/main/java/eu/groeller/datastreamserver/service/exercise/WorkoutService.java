e98afa016073cae8d02942e0f766eaad8b864c37
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-12-06 15:48:28 +0100

    Add WorkoutService/getWorkouts(User)

--- datastream-server/src/main/java/eu/groeller/datastreamserver/service/exercise/WorkoutService.java
+++ datastream-server/src/main/java/eu/groeller/datastreamserver/service/exercise/WorkoutService.java
@@ -8,14 +8,19 @@ import eu.groeller.datastreamserver.presentation.request.exercise.CreateWorkoutR
 import eu.groeller.datastreamserver.service.utils.DtoUtils;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class WorkoutService {
+    private static final Logger logger = LoggerFactory.getLogger(WorkoutService.class);
+
     private final WorkoutRepository workoutRepository;
     private final ExerciseRecordService exerciseRecordService;
 
@@ -34,5 +39,15 @@ public class WorkoutService {
 
         Workout workout = new Workout(user, request.date(), exerciseRecords);
         return workoutRepository.save(workout);
+    }
+
+    @Transactional(readOnly = true)
+    public Set<Workout> getWorkouts(@NonNull User user) {
+        logger.debug("Retrieving workouts for user: {}", user.getUsername());
+        
+        Set<Workout> workouts = workoutRepository.findByUser(user);
+        
+        logger.debug("Found {} workouts for user: {}", workouts.size(), user.getUsername());
+        return workouts;
     }
 }
