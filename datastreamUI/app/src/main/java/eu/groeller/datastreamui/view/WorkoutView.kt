c61447686e6381d95e6660dd6fc36e614ab98af4
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-12-07 23:09:41 +0100

    Remove WorkoutView

--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/view/WorkoutView.kt
+++
@@ -1,9 +0,0 @@
-package eu.groeller.datastreamui.view
-
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-
-@Composable
-fun workoutView() {
-}
