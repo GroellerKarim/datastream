866bb22142614227eb04b6f1582015242d4b45cd
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-12-09 21:28:44 +0100

    (ui) WorkoutRepository now properly sends request with pageable param

--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/workout/WorkoutRepository.kt
+++ datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/workout/WorkoutRepository.kt
@@ -3,11 +3,13 @@ package eu.groeller.datastreamui.data.workout
 import eu.groeller.datastreamui.User
 import eu.groeller.datastreamui.data.V1_PATH
 import eu.groeller.datastreamui.data.model.WorkoutResponse
+import eu.groeller.datastreamui.model.Slice
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.bearerAuth
 import io.ktor.client.request.get
 import io.ktor.http.isSuccess
+import io.ktor.http.parameters
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
@@ -17,12 +19,16 @@ class WorkoutRepository(private val httpClient: HttpClient, private val user: Us
 
     suspend fun getWorkouts(user: User): WorkoutState {
         val response = httpClient.get("${V1_PATH}/workouts") {
+            url {
+                parameters.append("page", "0")
+                parameters.append("size", "5")
+            }
             bearerAuth(user.token)
         }
 
         if (response.status.isSuccess()) {
-            val body: List<WorkoutResponse> =  response.body()
-            return WorkoutState.Success(body)
+            val body: Slice<WorkoutResponse> =  response.body()
+            return WorkoutState.Success(body.content)
         }
 
         return WorkoutState.Error(response.body())
