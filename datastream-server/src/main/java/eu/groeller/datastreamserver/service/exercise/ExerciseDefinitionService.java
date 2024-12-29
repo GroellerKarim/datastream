911488a4a76bc2ea6ab908ce792ae0c524552774
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-12-29 23:42:21 +0100

    Add ExerciseDefinitionService

--- datastream-server/src/main/java/eu/groeller/datastreamserver/service/exercise/ExerciseDefinitionService.java
+++ datastream-server/src/main/java/eu/groeller/datastreamserver/service/exercise/ExerciseDefinitionService.java
@@ -1,6 +1,8 @@
 package eu.groeller.datastreamserver.service.exercise;
 
+import eu.groeller.datastreamserver.domain.User;
 import eu.groeller.datastreamserver.domain.exercise.ExerciseDefinition;
+import eu.groeller.datastreamserver.domain.exercise.WorkoutType;
 import eu.groeller.datastreamserver.presentation.request.exercise.CreateExerciseDefinitionRequest;
 import eu.groeller.datastreamserver.persistence.exercise.ExerciseDefinitionRepository;
 import eu.groeller.datastreamserver.service.utils.DtoUtils;
@@ -8,6 +10,10 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,4 +31,12 @@ public class ExerciseDefinitionService {
 
         return exerciseDefinitionRepository.save(new ExerciseDefinition(request.name(), request.type()));
     }
+
+    public List<ExerciseDefinition> getExerciseDefinitionForTypeAndUser(User user, WorkoutType workoutType) {
+        return exerciseDefinitionRepository.findRecentExercisesForTypeAndUser(user, workoutType)
+                .stream()
+                .sorted(Comparator.comparing(ExerciseDefinition::getName))
+                .collect(Collectors.toList());
+    }
+
 }
