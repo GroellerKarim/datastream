d86eafa5b847f66d068d9376a278b08ff6c4ce22
GroellerKarim <karim.jonas.groeller@gmail.com>
2025-01-28 20:17:41 +0100

    (ui) Add addWorkoutType to workoutRepo
    Signed-off-by: GroellerKarim <karim.jonas.groeller@gmail.com>

--- datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/workout/WorkoutRepository.kt
+++ datastreamUI/app/src/main/java/eu/groeller/datastreamui/data/workout/WorkoutRepository.kt
@@ -3,11 +3,14 @@ package eu.groeller.datastreamui.data.workout
 import eu.groeller.datastreamui.User
 import eu.groeller.datastreamui.data.V1_PATH
 import eu.groeller.datastreamui.data.model.WorkoutResponse
+import eu.groeller.datastreamui.data.model.WorkoutType
 import eu.groeller.datastreamui.model.Slice
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.bearerAuth
 import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.http.isSuccess
 import io.ktor.http.parameters
 import kotlinx.coroutines.flow.Flow
@@ -32,5 +35,18 @@ class WorkoutRepository(private val httpClient: HttpClient, private val user: Us
         }
 
         return WorkoutState.Error(response.body())
+    }
+
+    suspend fun addWorkoutType(workoutType: String): WorkoutType? {
+        val response = httpClient.post("${V1_PATH}/workouts/workout-type") {
+            bearerAuth(user.token)
+            setBody(mapOf("name" to workoutType))
+        }
+
+        if(response.status.isSuccess()) {
+            return response.body()
+        }
+
+        return null
     }
 }
